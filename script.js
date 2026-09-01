b2e83098291bcd714d7552eddd9caa8f8ca1051e
Klay Muca <klay.muca@gmail.com>
2026-09-01 00:40:32 -0400

    Add city search interaction

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("JS is successfully connected!");
+const cityInput = document.querySelector(".city-input");  
+
+const searchButton = document.querySelector(".search-button");
+
+searchButton.addEventListener("click", function() {
+    
+    const city = cityInput.value;
+
+    if (city === "") {
+        document.querySelector(".error-message").textContent = "Please enter a city name.";
+    }
+
+});
